ebb6a63400b2633a35f663830250d6a2120759ce
Vasilena Stanoyska <v.stanoyska@gmail.com>
2024-12-21 18:44:08 +0200

    Create a virtual environment for the project
    Created a virtual environment for the project

--- travel_planner/user_trips/models.py
+++ travel_planner/user_trips/models.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class MyTrip(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name="trips"
     )
-    
+
     destination = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField()
-    
+    image_url = models.CharField(max_length=500, null=True, blank=True)
+
     # TODO: delete it, for now just for debugging if needed
     def __str__(self):
         return f"{self.destination} ({self.start_date} - {self.end_date})"
--- travel_planner/user_trips/views.py
+++ travel_planner/user_trips/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import MyTrip
@@ -22,6 +23,23 @@ def create_trip(request):
         if(start_date >= end_date):
             return redirect('list_trips')
         
+        api_url = "https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination"
+        headers = {
+            "x-rapidapi-key": "25aa21455dmsh86b46541b28213bp1489a5jsn374b2d97cd64",
+            "x-rapidapi-host": "booking-com15.p.rapidapi.com",
+        }
+        params = {"query": destination}
+        response = requests.get(api_url, headers=headers, params=params)        
+
+        if response.status_code == 200:
+            data = response.json()
+            if data.get("status") and data.get("data"):
+                image_url = data["data"][0].get("image_url", "")
+            else:
+                image_url = ""
+        else:
+            image_url = ""
+    
         MyTrip.objects.create(
             user=request.user,
             destination=destination,
